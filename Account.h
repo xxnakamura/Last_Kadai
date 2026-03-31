da3cf9f548e71b20092ca1ac49cbea9577b435ad
aipnobu <nobuo_iwata@aiphone.co.jp>
2026-03-31 15:37:48 +0900

    Update Account.h
    皆さんよろしく

--- Account.h
+++ Account.h
@@ -2,7 +2,7 @@
 #define ACCOUNT_H
 
 typedef struct {
-	int bank_account_number;      // 口座番号
+	int id;      // 口座番号
 	char name[20];  // 名前
 	int password;       // パスワード
 	int balance;       // 残高
@@ -10,13 +10,13 @@ typedef struct {
 	int failure_times;       // 失敗回数
 	} ACCOUNT;
 	
-void add_account(ACCOUNT a);      // 口座登録
+void add_account(ACCOUNT account);      // 口座登録
 void prt_account(int id);     // 口座表示
 	ACCOUNT* find(int id);        // 口座検索
 
-int  wd(ACC *a, int m);   // 出金実行
-int  chk_m(int m);        // 金額検証
-int  auth(ACC *a, int pw);// パスワード照合
+int  wd(ACC *account, int money);   // 出金実行
+int  chk_m(int money);        // 金額検証
+int  auth(ACC *account, int pw);// パスワード照合
 int  get_fee(int h);        // 手数料計算
 
 #define MAX_A 10
