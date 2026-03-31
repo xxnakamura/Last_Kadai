56230c20a7c9693cfd5af425a1fe99f297eda8aa
terao2649 <shohei_terao@aiphone.co.jp>
2026-03-31 15:45:42 +0900

    Security.cを修正しました

--- Security.c
+++ Security.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include "Account.h"
 
-int auth(ACC *a, int pw) {
-    if (a->p == pw) {
-        a->l = 0;
+int auth(ACCOUNT *account, int pw) {
+    if (account -> password == pw) {
+        account -> l = 0;
         return 1;
     } else {
-        a->l++;
-        if (a->l >= 3) a->st = 0; 
+        account -> l++;
+        if (account -> l >= 3){
+			account -> status = 0;
+		}
         return 0;
     }
 }
 
-int get_f(int h) {
+int get_fee(int h) {
     if (h < 9 || h >= 18) {
         return 110; 
     }
